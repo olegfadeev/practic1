091ee908963d93c35570cf0eda205bf8b932c5c7
axxaxaaxaxaxaaaxxa <70947572+olegfadeev@users.noreply.github.com>
2020-09-08 15:26:28 +0300

    Add files via upload

---
+++ softstring.cs
@@ -0,0 +1,12 @@
+﻿using System;
+struct practice
+{
+  static void Main(string[] args)
+  {
+    string s = "Microsoft .NET Framework 2 Application Development Foundation";
+    string[] sa = s.Split(" ");
+    Array.Sort(sa);
+    s = string.Join(" ", sa);
+    Console.WriteLine(s);
+  }
+}
